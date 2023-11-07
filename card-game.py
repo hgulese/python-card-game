1a964c671e64e33881172342796ea8164ed17fd9
hgulese <hgulese02@gmail.com>
2023-11-07 15:30:46 +0300

    Deck class created.

--- card-game.py
+++ card-game.py
@@ -10,8 +10,22 @@ class Card:
         return f"{self.type} {self.value}"
     
         
-clubs5 = Card("Clubs", "5")
-diamondsA = Card("Diamonds", "A")
+# clubs5 = Card("Clubs", "5")
+# diamondsA = Card("Diamonds", "A")
 
 # print(clubs5.getKart())
-print(diamondsA)
+# print(diamondsA)
+
+class Deck:
+    types = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    
+    def __init__(self):
+        self.cards = [Card(type, value) for type in Deck.types for value in Deck.values]
+        
+        # for type in types:
+        #     for value in values:
+        #         self.cards.append(Card(type, value))
+        print(self.cards)
+
+Deck()
